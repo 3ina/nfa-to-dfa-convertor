7d23836a08bf3e2f8891ecbc8dd436dcdb4b725f
sinaroydel <3inaroydl@gmail.com>
2024-10-10 14:11:43 +0330

    add contains helper method

--- automata/helper.go
+++ automata/helper.go
@@ -9,3 +9,13 @@ type Transition struct {
 	Input rune
 	to    []*State
 }
+
+func contains(states []*State, target *State) bool {
+	for _, state := range states {
+		if state == target {
+			return true
+		}
+	}
+
+	return false
+}
