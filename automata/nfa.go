8ca54865eed19e82af15616d2ad0a0fc6e7893d4
sinaroydel <3inaroydl@gmail.com>
2024-10-20 12:39:06 +0330

    add epsilonClosure method to nfa struct

--- automata/nfa.go
+++ automata/nfa.go
@@ -7,3 +7,33 @@ type NFA struct {
 	StartState  *State
 	FinalStates []*State
 }
+
+func (nfa *NFA) epsilonClosure(states []*State) []*State {
+	closure := make([]*State, 0)
+	visited := make(map[*State]bool)
+
+	var stack []*State
+	stack = append(stack, states...)
+
+	for len(stack) > 0 {
+		state := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if !visited[state] {
+			visited[state] = true
+			closure = append(closure, state)
+			for _, t := range nfa.Transitions {
+				if t.From == state && t.Input == 'ε' {
+					for _, toState := range t.to {
+						if !visited[toState] {
+							stack = append(stack, toState)
+						}
+					}
+
+				}
+			}
+		}
+	}
+
+	return closure
+}
